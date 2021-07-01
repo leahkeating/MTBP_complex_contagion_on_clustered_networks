0ed16a9bdf8038ee0ca65add22f7d5e8e817c587
leahkeating <l.keating97@gmail.com>
2021-07-01 10:15:17 +0100

    new file

---
+++ cascade_condition.R
@@ -0,0 +1,7 @@
+########################################################################
+# Title: cascade_condition.R
+# Purpose: code to determine parameters which result in sub- and supercritical cascades
+# Author: Leah Keating
+# Date last edited: 1 July 2021
+########################################################################
+
