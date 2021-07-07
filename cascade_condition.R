91e531f77b9bcde2e2e39dd6da1a3230fd7070b4
leahkeating <l.keating97@gmail.com>
2021-07-07 09:09:52 +0100

    adding functions for criticality

--- cascade_condition.R
+++ cascade_condition.R
@@ -2,6 +2,66 @@
 # Title: cascade_condition.R
 # Purpose: code to determine parameters which result in sub- and supercritical cascades
 # Author: Leah Keating
-# Date last edited: 1 July 2021
+# Date last edited: 6 July 2021
 ########################################################################
+library(doParallel)
 
+# load the functions - we need the mean_mat function in particular
+source("mtbp_functions.R")
+
+# initialise a data frame with the alpha, q1 values we are interested in
+# m is the number of triangles, n is the number of single edges, cl_4 is the
+# number of 4-cliques
+
+criticality.df <- expand.grid(alpha = seq(from = 0, to = 1, by = 0.01),
+            q1 = seq(from = 0.7, to = 0.9, by = 0.01)) %>%
+  as_tibble()
+
+# function to calculate the criticality
+# returns True if subcritical, false if supercritical
+
+criticality <- function(alpha, q1, m, n, cl_4 = 0){
+  x <- mean_mat(alpha = alpha, q1 = q1, m = m, n = n, cl_4 = cl_4)
+  lambda <- Re(eigen(x)$values) %>% max()
+  if (lambda>1){
+    return(FALSE)
+  }else{
+    return(TRUE)
+  }
+}
+
+# next we want to calculate the criticality for each network type
+# for all parameter combinations
+
+# do this in parallel
+
+cluster <- makeCluster(6) # run on 6 cores
+registerDoParallel(cluster)
+
+# 6 2-cliques
+
+crit_6_2cl <- foreach(i=1:nrow(criticality.df), .combine = "c", .packages = c("Matrix", "dplyr")) %dopar%
+  criticality(alpha = criticality.df$alpha[i], q1 = criticality.df$q1[i], m = 0, n = 6, cl_4 = 0)
+
+# m = 1, n = 4
+
+crit_4_2cl_1_3cl <- foreach(i=1:nrow(criticality.df), .combine = "c", .packages = c("Matrix", "dplyr")) %dopar%
+  criticality(alpha = criticality.df$alpha[i], q1 = criticality.df$q1[i], m = 1, n = 4, cl_4 = 0)
+
+# 3 3-cliques
+
+crit_3_3cl <- foreach(i=1:nrow(criticality.df), .combine = "c", .packages = c("Matrix", "dplyr")) %dopar%
+  criticality(alpha = criticality.df$alpha[i], q1 = criticality.df$q1[i], m = 3, n = 0, cl_4 = 0)
+
+# 2 4-cliques
+
+crit_2_4cl <- foreach(i=1:nrow(criticality.df), .combine = "c", .packages = c("Matrix", "dplyr")) %dopar%
+  criticality(alpha = criticality.df$alpha[i], q1 = criticality.df$q1[i], m = 0, n = 0, cl_4 = 2)
+
+stopImplicitCluster()
+
+# create a data frame where for each network True indicates that those parameters lead to subcritical
+# diffusion and False means that there is supercritical diffusion
+
+
+criticality.df <- criticality.df %>% mutate(crit_6_2cl, crit_4_2cl_1_3cl, crit_3_3cl, crit_2_4cl)
